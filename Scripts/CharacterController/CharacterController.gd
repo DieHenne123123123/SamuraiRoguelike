d6a0ac1915658ced3da03869c5993b5aef46b97e
Die_Henne <hendrikschroedter@web.de>
2024-07-11 15:44:33 +0200

    Adding the option to jump less by releasing Space

--- Scripts/CharacterController/CharacterController.gd
+++ Scripts/CharacterController/CharacterController.gd
@@ -2,9 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const JUMP_VELOCITY = -600.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
@@ -14,11 +12,10 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_released("Jump") && velocity.y <= 0:
+		velocity.y = -20
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
