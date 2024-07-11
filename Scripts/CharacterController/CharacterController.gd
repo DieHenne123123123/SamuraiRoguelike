a980922dff992880f801ce892a9bf7cb0955f31c
Die_Henne <hendrikschroedter@web.de>
2024-07-11 15:56:19 +0200

    Finished CharacterMovement

--- Scripts/CharacterController/CharacterController.gd
+++ Scripts/CharacterController/CharacterController.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -650.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
@@ -15,11 +15,10 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_released("Jump") && velocity.y <= 0:
-		velocity.y = -20
+		velocity.y = -50
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
